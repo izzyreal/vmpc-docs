0442bcaa3aea4b1eda5bb0dd23565c4185bf43e0
Izmar Verhage <izmaelverhage@gmail.com>
2025-01-31 13:43:21 +0100

    Bump version to 0.9

--- conf.py
+++ conf.py
@@ -41,9 +41,9 @@ extensions = [
 ]
 
 # The short X.Y version.
-version = "0.5"
+version = "0.9"
 # The full version, including alpha/beta/rc tags.
-release = u'0.5.8'
+release = u'0.9.0'
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 if not os.path.exists(os.path.join(dir_path, "versions.json")):
